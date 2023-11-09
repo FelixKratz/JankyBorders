470f75628380f8bde10967933ea18bf959d06afd
Felix Kratz <felix.kratz@tu-dortmund.de>
2023-11-09 08:12:31 +0100

    fix border deactivate on attached window creation

--- src/events.c
+++ src/events.c
@@ -33,8 +33,8 @@ static void window_spawn_handler(uint32_t event, struct window_spawn_data* data,
   if (event == EVENT_WINDOW_CREATE) {
     if (windows_window_create(windows, wid, sid)) {
       debug("Window Created: %d %d\n", wid, sid);
+      windows_window_focus(windows, get_front_window(cid));
     }
-    windows_window_focus(windows, get_front_window(cid));
   } else if (event == EVENT_WINDOW_DESTROY) {
     if (windows_window_destroy(windows, wid, sid)) {
       debug("Window Destroyed: %d %d\n", wid, sid);
